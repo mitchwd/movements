595d2081530f9a36fffd73d6a3d1c21a150e0215
@mitchwd <mitch@mitchwd.com>
2015-09-04 12:54:46 +0930

    Added sign_in route (JSON Only)
    SAMPLE REQUEST: curl -s -o /dev/null -w "%{http_code}" -H
    "Content-Type: application/json" -d '{"movement": {"location_id": "1",
    "identifier_code": “234”}}’ http://localhost:3000/sign_in.json

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -33,6 +33,25 @@ class MovementsController < ApplicationController
     end
   end
 
+  # POST /sign_in.json
+  # Requests @movement.location_id and @movement.identifier_code
+  # SAMPLE REQUEST:
+    # curl -s -o /dev/null -w "%{http_code}" -H "Content-Type: application/json" -d '{"movement": {"location_id": "1", "identifier_code": "234"}}' http://localhost:3000/sign_in.json
+    # -> 201 (Success: Record created)
+  def sign_in
+    @movement = Movement.new
+    @movement.location = Location.where(id: movement_params[:location_id]).take!
+    @movement.person = Identifier.where(code: movement_params[:identifier_code]).take!.person
+
+    respond_to do |format|
+      if @movement.save
+        format.json { render :show, status: :created, location: @movement }
+      else
+        format.json { render json: @movement.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movement
@@ -41,6 +60,6 @@ class MovementsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movement_params
-      params.require(:movement).permit(:person_id, :location_id, :notes)
+      params.require(:movement).permit(:person_id, :location_id, :notes, :identifier_code)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :people do
     collection { post :import }
   end
+  post 'sign_in' => "movements#sign_in"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
